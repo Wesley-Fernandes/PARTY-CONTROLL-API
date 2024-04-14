f1abeb7b4fc71623f41b9dc8c69d0bab1f7b1ea3
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-04-14 05:13:14 -0300

    erase: swaggerjs types and dist

--- dist/Controller/GUEST/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const create_1 = require("./create");
-const erase_1 = require("./erase");
-const find_1 = require("./find");
-const update_1 = require("./update");
-exports.default = { create: create_1.create, erase: erase_1.erase, find: find_1.find, update: update_1.update };
--- dist/Controller/PARTY/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const all_1 = require("./all");
-const create_1 = require("./create");
-const erase_1 = require("./erase");
-const find_1 = require("./find");
-const update_1 = require("./update");
-exports.default = { create: create_1.create, find: find_1.find, erase: erase_1.erase, all: all_1.all, update: update_1.update };
